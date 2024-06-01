61718958f6d088e996b51c53596dadadc4646c18
EliasCode79 <eliasfranco2379@gmail.com>
2024-06-01 11:01:05 -0400

    interfaces en typescript

--- 01-bases/src/bases/09-fecth-api.ts
+++ 01-bases/src/bases/09-fecth-api.ts
@@ -1,8 +1,11 @@
+import { GIFResponse } from '../interfaces/gif.response';
+
 const apiKey = 'ZsvDWspQk3I4PbiqRRem51KjtMgSVYo6';
 
 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
 	.then((res) => res.json())
-	.then(({ data }) => {
-		console.log({ data });
+	.then((body: GIFResponse) => {
+		// indicamos q el body un type de GifResponse
+		console.log(body.data.url);
 	})
 	.catch((error) => console.log(error));
